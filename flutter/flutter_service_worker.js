1fabf5fe7526f87d49fd08facadbb5094265ea71
Bob Willer <bob.willer@hotmail.com>
2021-08-06 21:04:49 -0400

    'note'

--- flutter/flutter_service_worker.js
+++ flutter/flutter_service_worker.js
@@ -50,9 +50,9 @@ const RESOURCES = {
 "icons/favicon.ico": "0bf4140b7b6fbdb3ff8a304b4d650c0d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "798f2afbdb1aadcbc3cd293fd7f5fb49",
-"/": "798f2afbdb1aadcbc3cd293fd7f5fb49",
-"main.dart.js": "35bebc709271f63007be31fe4a04bdb1",
+"index.html": "75d4e53cb6f634ebd3b3e02a84178152",
+"/": "75d4e53cb6f634ebd3b3e02a84178152",
+"main.dart.js": "2bdd0156e9ccd72eeaa7275abf20fc9c",
 "manifest.json": "e680a96360a9e49c11b40e202abce0fd",
 "version.json": "6dac297582193241d3d0da2f37870beb"
 };
